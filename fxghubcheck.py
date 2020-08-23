26cdab488a99adc5eae39640ce7eb393105bfc11
Celticcow <gregory.dunlap@gmail.com>
2020-08-23 10:55:16 -0500

    start files

---
+++ fxghubcheck.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3 -W ignore::DeprecationWarning
+
+import requests
+import json
+import sys
+import time
+import ipaddress
+import apifunctions
+import cgi,cgitb
+
+#remove the InsecureRequestWarning messages
+import urllib3
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+"""
+Greg_Dunlap / CelticCow
+"""
+
+def main():
+    print("in_function_main")
+
+if __name__ == "__main__":
+    main()
+#end of program
